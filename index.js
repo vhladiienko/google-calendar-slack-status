6bdc45f9dd635805f7b1eb8c4c83bab580123345
Vitalii Hladiienko <vitalii@mailbox.org>
2020-11-02 00:51:25 +0200

    Removed debug

--- index.js
+++ index.js
@@ -61,8 +61,7 @@ app.post('/', (req, res, next) => {
   if (process.env.AM_PM === true) {
     status = `${status} from ${start.format('hh:mm')} to ${end.format('hh:mm A')} (${process.env.TIME_ZONE})`;
   } else {
-    // status = `${status} from ${start.format('HH:mm')} to ${end.format('HH:mm')} (${process.env.TIME_ZONE})`;
-    status = `DEBUG: ${JSON.stringify(req.body)}`;
+    status = `${status} from ${start.format('HH:mm')} to ${end.format('HH:mm')} (${process.env.TIME_ZONE})`;
   }
   let profile = JSON.stringify({
     "status_text": status,
